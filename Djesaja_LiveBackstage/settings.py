3ed6e87907f74677a19e8f00c94b1f4f279fa101
aru-01 <arifu5758@gmail.com>
2026-02-05 05:17:20 +0600

    minor update

--- Djesaja_LiveBackstage/settings.py
+++ Djesaja_LiveBackstage/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ccqpn8d5e(y25=%w8p35h$_(lb$0!ee)1&bpt6pb8us807b65)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
